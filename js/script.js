2c021fc1437a0b41d620a5365ed80e4aded5df03
Albab47 <115328782+Albab47@users.noreply.github.com>
2024-02-18 17:34:49 +0600

    Add event listener to all seat btn

--- js/script.js
+++ js/script.js
@@ -1,16 +1,47 @@
-
-
 // initial numeric values
 let availableSeats = 40;
 let numberOfTicket = 0;
 let totalPrice = 0;
 let grandPrice = 0;
 const ticketPrice = 550;
+let selectedSeat = 0;
 
 // get ref from element
-const availableSeatsText = document.getElementById('availableSeats');
-const numberOfTicketText = document.getElementById('numberOfTicket');
-const totalPriceText = document.getElementById('totalPrice');
-const grandTotalText = document.getElementById('grandTotal');
+const availableSeatsText = document.getElementById("availableSeats");
+const numberOfTicketText = document.getElementById("numberOfTicket");
+const totalPriceText = document.getElementById("totalPrice");
+const grandTotalText = document.getElementById("grandTotal");
+const orderItems = document.getElementById("orderItems");
+const seats = document.querySelectorAll(".seat");
 
-
+// iterate through all seat to add click event
+for (const seat of seats) {
+  seat.addEventListener("click", (event) => {
+    selectedSeat++;
+    if (selectedSeat <= 4) {
+      seat.style.backgroundColor = "#1DD100";
+      seat.style.color = "#fff";
+      const seatName = event.target.innerText;
+      const li = document.createElement("li");
+      const h6 = document.createElement("h6");
+      const p = document.createElement("p");
+      const span = document.createElement("span");
+      h6.textContent = seatName;
+      p.textContent = "Economy";
+      span.textContent = ticketPrice;
+      li.appendChild(h6);
+      li.appendChild(p);
+      li.appendChild(span);
+      li.classList.add("flex", "justify-between");
+      orderItems.appendChild(li);
+      availableSeats--;
+      numberOfTicket++;
+      totalPrice += ticketPrice;
+      grandPrice += ticketPrice;
+      availableSeatsText.innerText = availableSeats;
+      numberOfTicketText.innerText = numberOfTicket;
+      totalPriceText.innerText = totalPrice;
+      grandTotalText.innerText = grandPrice;
+    }
+  });
+}
