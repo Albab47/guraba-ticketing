8146c1b811fb7d0a9e2dc10af7fedaf0bbee6ef4
Albab47 <115328782+Albab47@users.noreply.github.com>
2024-02-18 16:13:53 +0600

    assign initial values and element ref to script.js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+
+// initial numeric values
+let availableSeats = 40;
+let numberOfTicket = 0;
+let totalPrice = 0;
+let grandPrice = 0;
+const ticketPrice = 550;
+
+// get ref from element
+const availableSeatsText = document.getElementById('availableSeats');
+const numberOfTicketText = document.getElementById('numberOfTicket');
+const totalPriceText = document.getElementById('totalPrice');
+const grandTotalText = document.getElementById('grandTotal');
+
+
